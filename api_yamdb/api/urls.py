698c720f843a1dd7c549f22c9832eeb36abec110
Allex-Nik <alexandr.nikolaev96@gmail.com>
2023-07-05 12:58:43 +0400

    2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -11,11 +11,7 @@ router.register(r'users', UserViewSet)
 router.register('titles', TitleViewSet, basename='titles')
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('genres', GenreViewSet, basename='genres')
-# router.register(
-#     r'titles/(?P<title_id>\d+)/reviews)',
-#     ReviewViewSet,
-#     basename='reviews'
-# )
+
 
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
